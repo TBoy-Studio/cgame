1c090de9a2d39e66aae1c065022d1ff2fefae59a
Thijs van Dijk <thijs3214@gmail.com>
2023-08-22 20:09:17 +0200

    Added ability to make custom KeyActionLibs

---
+++ include/input.h
@@ -0,0 +1,48 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+// 3rd party lib
+#include <GLFW/glfw3.h>
+
+// Including string.h for memcpy operation
+#include <string.h>
+
+/*! @brief The function pointer type for a single keyboard key callback.
+ *
+ *  @param[in] action `GLFW_PRESS`, `GLFW_RELEASE` or `GLFW_REPEAT`.
+ *  @param[in] mods Bit field describing which modifier keys (SHIFT, CONTROL etc) were
+ *  held down. See more about these in the glfw documentation. 
+ */
+typedef void (* KeyAction)(int action, int mods);
+
+/*! @brief  The KeyActionLib is a typedef for an array of KeyAction function pointers.
+
+    The size of this array is equal to the last GLFW key code to allow for easy accessing of
+    elements. For instance: to get the KeyAction associated with the keyboard letter E
+*/
+typedef KeyAction KeyActionLib[GLFW_KEY_LAST + 1];
+
+// The current key action library that the callbacks will be fetched from
+extern KeyActionLib current_key_action_lib;
+
+/*! @brief Initializes the input library with a certain KeyActionLib.
+
+    @param[in] window The window to fetch inputs from
+    @param[in] key_action_lib The array of function pointers from individual key actions
+*/
+void input_initialize(GLFWwindow* window, KeyActionLib key_action_lib);
+
+/*! @brief Copies the specified KeyActionLib over the current KeyActionLib.
+
+    @param[in] new_key_action_lib The new KeyActionLib to copy over the old one
+*/
+void input_update_key_action_lib(KeyActionLib new_key_action_lib);
+
+/*! @brief Updates the KeyAction associated with a single key
+
+    @param[in] key The key to update the KeyAction of
+    @param[in] new_key_action The new key action to be associated with this key
+*/
+void input_update_key_action(int key, KeyAction new_key_action);
+
+#endif
---
+++ src/input.c
@@ -0,0 +1,25 @@
+#include <input.h>
+
+KeyActionLib current_key_action_lib;
+
+void input_key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
+    if(key < 0) return;                                             // If key is unknown simply ignore
+    if(current_key_action_lib[(unsigned int)key] != NULL)
+    {
+        current_key_action_lib[(unsigned int)key](action, mods);    // If this key has an associated action, execute it
+    } 
+}
+
+void input_initialize(GLFWwindow* window, KeyActionLib key_action_lib){
+    glfwSetKeyCallback(window, input_key_callback); // Set generic key callback
+    input_update_key_action_lib(key_action_lib);    // Set the array of function pointers per key
+}
+
+void input_update_key_action_lib(KeyActionLib new_key_action_lib){
+    memcpy(current_key_action_lib, new_key_action_lib, sizeof(KeyActionLib));   // Simply copy the new KeyActionLib over the old one
+}
+
+void input_update_key_action(int key, KeyAction new_key_action){
+    if(key < 0 || key > GLFW_KEY_LAST) return;                  // If key outside range, ignore
+    current_key_action_lib[(unsigned int)key] = new_key_action; // Update function pointer
+}
