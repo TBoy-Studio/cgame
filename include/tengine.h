e1f90db2cc3ecbcbc1a0d0be8448fa22849c734a
Thijs van Dijk <thijs3214@gmail.com>
2023-08-22 20:35:01 +0200

    Added easy include file for parent projects

---
+++ include/tengine.h
@@ -0,0 +1,13 @@
+/*
+    Easy include file to include all parts of the tengine library
+*/
+
+#ifndef TENGINE_H
+#define TENGINE_H
+
+#include <window.h>
+#include <camera.h>
+#include <input.h>
+#include <shader.h>
+
+#endif
