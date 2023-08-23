3f00aed6b9ae993240e68af8c45b28e09ba38dcc
Thijs van Dijk <thijs3214@gmail.com>
2023-08-23 18:20:30 +0200

    Added per frame key input handling and mouse cursor handling

--- include/input.h
+++ include/input.h
@@ -33,19 +33,28 @@ typedef void (* MouseButtonAction)(GLFWwindow* window, int action, int mods);
 */
 typedef void (* MouseScrollAction)(GLFWwindow* window, double xoffset, double yoffset);
 
-/*! @brief  The KeyActionLib is a typedef for an array of KeyAction function pointers.
-
-    The size of this array is equal to the last GLFW key code to allow for easy accessing of
-    elements
+/*! @brief The function pointer type for a single mouse cursor callback
+ *
+ *  @param[in] window The window the event occured in
+ *  @param[in] xpos The x-offset of the scroll (can be usually be ignored as scroll happens along y axis)
+ *  @param[in] ypos The y-offset of the scroll (The amount the user has scrolled)
 */
-typedef KeyAction KeyActionLib[GLFW_KEY_LAST + 1];
+typedef void (* MouseCursorAction)(GLFWwindow* window, double xpos, double ypos);
 
-/*! @brief  The MouseButtonActionLib is a typedef for an array of MouseButtonAction function pointers.
-
-    The size of this array is equal to the last GLFW mouse button code to allow for easy accessing of
-    elements
+/*! @brief A type that provides information ont he key callback that has been set up on this key
+ *  
+ *  Holds the callback function pointer, whether or not the press event should be repeated every
+ *  frame (for holding down keys) and the current key state (GLFW_PRESS or GLFW_RELEASE)
 */
-typedef MouseButtonAction MouseButtonActionLib[GLFW_MOUSE_BUTTON_LAST + 1];
+typedef struct{
+    KeyAction action;
+    int mods;
+    unsigned char repeat;
+    unsigned char last_key_action;
+} KeyActionContext;
+
+// Variables to easily access the previous x and y of the mouse cursor
+extern double input_mouse_cursor_last_x, input_mouse_cursor_last_y;
 
 /*! @brief Initializes the input library.
 
@@ -57,20 +66,20 @@ void input_initialize(GLFWwindow* window);
 
     @param[in] new_key_action_lib The new KeyActionLib to copy over the old one
 */
-void input_set_key_action_lib(KeyActionLib new_key_action_lib);
+void input_set_key_action_lib(KeyActionContext new_key_action_lib[GLFW_KEY_LAST + 1]);
 
 /*! @brief Updates the KeyAction associated with a single key
 
     @param[in] key The key to update the KeyAction of
     @param[in] new_key_action The new key action to be associated with this key
 */
-void input_set_key_action(int key, KeyAction new_key_action);
+void input_set_key_action(int key, KeyAction new_key_action, unsigned char repeat);
 
 /*! @brief Copies the specified MouseButtonActionLib over the current MouseButtonActionLib.
 
     @param[in] new_mouse_button_action_lib The new KeyActionLib to copy over the old one
 */
-void input_set_mouse_button_action_lib(MouseButtonActionLib new_mouse_button_action_lib);
+void input_set_mouse_button_action_lib(MouseButtonAction new_mouse_button_action_lib[GLFW_MOUSE_BUTTON_LAST + 1]);
 
 /*! @brief Updates the MouseButtonAction associated with a single mouse button
 
@@ -84,5 +93,17 @@ void input_set_mouse_button_action(int button, MouseButtonAction new_key_action)
     @param[in] new_scroll_action The new action to be associated with scrolling
 */
 void input_set_mouse_scroll_action(MouseScrollAction new_scroll_action);
+
+/*! @brief Update the MouseCursorAction associated with moving the mouse
+ *  
+ *  @param[in] new_cursor_action The new action to be associated with moving the mouse
+*/
+void input_set_mouse_cursor_action(MouseCursorAction new_cursor_action);
+
+/*! @brief Handles per frame callbacks for keys that are set up for that
+
+    @param[in] window The window to process the keys for
+*/
+void input_process(GLFWwindow* window);
 
 #endif
--- src/input.c
+++ src/input.c
@@ -1,17 +1,36 @@
 #include <input.h>
 
-KeyAction current_key_actions[GLFW_KEY_LAST + 1];
+KeyActionContext current_key_actions[GLFW_KEY_LAST + 1];
 MouseButtonAction current_mouse_button_actions[GLFW_MOUSE_BUTTON_LAST + 1];
 MouseScrollAction current_scroll_action;
+MouseCursorAction current_cursor_action;
+
+double input_mouse_cursor_last_x, input_mouse_cursor_last_y;
+unsigned char first_mouse = 1;
+
+void input_process(GLFWwindow* window){
+    // Go through each key
+    for(unsigned short i = 0; i <= GLFW_KEY_LAST; i++){
+        KeyActionContext context = current_key_actions[i];
+        if(context.last_key_action == GLFW_PRESS && context.repeat == GLFW_TRUE){
+            // If it is currently being pressed and the repeat bool is set, execute the callback function
+            context.action(window, GLFW_PRESS, current_key_actions[i].mods);
+        }
+    }
+}
 
 void input_key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
     // If key is unknown simply ignore
     if(key < 0 || key > GLFW_KEY_LAST) return;
 
-    // If this key has an associated action, execute it
-    if(current_key_actions[(unsigned int)key] != NULL)
+    KeyActionContext* context = &current_key_actions[(unsigned int)key];
+
+    // If this key has an associated action and is not a repeat, execute it
+    if(context->action != NULL && action != GLFW_REPEAT)
     {
-        current_key_actions[(unsigned int)key](window, action, mods);
+        context->last_key_action = (unsigned char)action;
+        context->mods = mods;
+        context->action(window, action, mods);
     } 
 }
 
@@ -31,33 +50,52 @@ void input_scroll_callback(GLFWwindow* window, double xoffset, double yoffset){
     if(current_scroll_action != NULL) current_scroll_action(window, xoffset, yoffset);
 }
 
+void input_cursor_callback(GLFWwindow* window, double xpos, double ypos){
+    // Fix first mouse input
+    if(first_mouse){
+        input_mouse_cursor_last_x = xpos;
+        input_mouse_cursor_last_y = ypos;
+        first_mouse = 0;
+    }
+
+    // If there's a cursor action registered, execute it
+    if(current_cursor_action != NULL) current_cursor_action(window, xpos, ypos);
+
+    // Update the last coordinates with the most recent ones after all calculation is done
+    input_mouse_cursor_last_x = xpos;
+    input_mouse_cursor_last_y = ypos;
+}
+
 void input_initialize(GLFWwindow* window){
     // Set generic key callback
     glfwSetKeyCallback(window, input_key_callback);
     glfwSetMouseButtonCallback(window, input_mouse_button_callback);
     glfwSetScrollCallback(window, input_scroll_callback);
+    glfwSetCursorPosCallback(window, input_cursor_callback);
 
     // Make sure to set all function pointers to zero
     // Prevents accidentally interpreting a random value as a function to execute
-    memset(current_key_actions, 0x0, sizeof(KeyAction) * (GLFW_KEY_LAST + 1));
+    memset(current_key_actions, 0x0, sizeof(KeyActionContext) * (GLFW_KEY_LAST + 1));
     memset(current_mouse_button_actions, 0x0, sizeof(MouseButtonAction) * (GLFW_MOUSE_BUTTON_LAST + 1));
     current_scroll_action = 0x0;
+    current_cursor_action = 0x0;
 }
 
-void input_set_key_action_lib(KeyAction new_key_actions[GLFW_KEY_LAST + 1]){
+void input_set_key_action_lib(KeyActionContext new_key_actions[GLFW_KEY_LAST + 1]){
     // Simply copy the new KeyActions over the old ones
-    memcpy(current_key_actions, new_key_actions, sizeof(KeyAction) * (GLFW_KEY_LAST + 1));   
+    memcpy(current_key_actions, new_key_actions, sizeof(KeyActionContext) * (GLFW_KEY_LAST + 1));   
 }
 
-void input_set_key_action(int key, KeyAction new_key_action){
+void input_set_key_action(int key, KeyAction new_key_action, unsigned char repeat){
     // If key outside range, ignore
     if(key < 0 || key > GLFW_KEY_LAST) return;
 
     // Update function pointer
-    current_key_actions[(unsigned int)key] = new_key_action;
+    current_key_actions[(unsigned int)key].action = new_key_action;
+    current_key_actions[(unsigned int)key].repeat = repeat;
 }
 
-void input_set_mouse_button_action_lib(MouseButtonActionLib new_mouse_button_actions){
+void input_set_mouse_button_action_lib(MouseButtonAction new_mouse_button_actions[GLFW_MOUSE_BUTTON_LAST + 1]){
     // Simply copy the new MouseButtonActions over the old ones
     memcpy(current_mouse_button_actions, new_mouse_button_actions, sizeof(MouseButtonAction) * (GLFW_MOUSE_BUTTON_LAST + 1));
 }
@@ -72,4 +110,8 @@ void input_set_mouse_button_action(int button, MouseButtonAction new_mouse_butto
 
 void input_set_mouse_scroll_action(MouseScrollAction new_scroll_action){
     current_scroll_action = new_scroll_action;
+}
+
+void input_set_mouse_cursor_action(MouseCursorAction new_cursor_action){
+    current_cursor_action = new_cursor_action;
 }
