35f93a62baa1d4df176beba4487eee25d5f45c8e
Thijs van Dijk <thijs3214@gmail.com>
2023-08-24 21:26:03 +0200

    Changed camera system to new naming convention

--- include/camera.h
+++ include/camera.h
@@ -5,15 +5,12 @@
 #include <cglm/vec3.h>
 #include <cglm/cam.h>
 
-#define NEAR_PLANE_DIST 0.1f
-#define FAR_PLANE_DIST 200.0f
-
 typedef enum{
     FORWARD,
     BACKWARD,
     LEFT,
     RIGHT
-} CameraMovement;
+} Camera_MovementEnum;
 
 typedef struct{
     vec3 position;
@@ -38,31 +35,31 @@ typedef struct{
     Other values of camera will use default values
     Stores resulting camera in result
 */
-void camera_create(float pos_x, float pos_y, float pos_z, float up_x, float up_y, float up_z, float yaw, float pitch, float aspect_ratio, Camera* result);
+void Camera_placeCamera(vec3 pos, vec3 up, vec3 front, float yaw, float pitch, float aspect_ratio);
 
 /*
     Gets the projection matrix for a perspective view from this camera and stores it in result
 */
-void camera_get_perspective(Camera* cam, mat4 result);
+void Camera_getProjectionMatrix(mat4 result);
 
 /*
     Gets the view matrix of the camera in it's current state and stores it in result
 */
-void camera_get_view_matrix(Camera* cam, mat4 result);
+void Camera_getViewMatrix(mat4 result);
 
 /*
     Handles updates of camera position caused by keyboard input
 */
-void camera_process_keyboard_movement(Camera* cam, CameraMovement movement, float delta_time);
+void Camera_processKeyboardMovement(Camera_MovementEnum movement, float delta_time);
 
 /*
     Handles updates of camera pitch and yaw caused by mouse input
 */
-void camera_process_mouse_movement(Camera* cam, float x_offset, float y_offset, unsigned char constrain_pitch);
+void Camera_processMouseMovement(float x_offset, float y_offset, unsigned char constrain_pitch);
 
 /*
     Handles updates to camera zoom caused by scrolling the mouse wheel
 */
-void camera_process_mouse_scroll(Camera* cam, float y_offset);
+void Camera_processMouseScroll(float y_offset);
 
 #endif
--- src/camera.c
+++ src/camera.c
@@ -1,108 +1,111 @@
 #include <camera.h>
 
-const float YAW = -90.0f;
-const float PITCH = 0.0f;
-const float SPEED = 5.0f;
-const float SENSITIVITY = 0.1f;
-const float ZOOM = 45.0f;
+Camera camera;
 
-void update_camera_vectors(Camera* cam){
+static const float NEAR_PLANE_DIST = 0.1f;
+static const float FAR_PLANE_DIST = 200.0f;
+static const float SPEED = 5.0f;
+static const float SENSITIVITY = 0.1f;
+static const float ZOOM = 45.0f;
+
+static void _updateCameraVectors(void)
+{
     // Calculate new front vector
-    cam->front[0] = cos(glm_rad(cam->yaw)) * cos(glm_rad(cam->pitch));
-    cam->front[1] = sin(glm_rad(cam->pitch));
-    cam->front[2] = sin(glm_rad(cam->yaw)) * cos(glm_rad(cam->pitch));
-    glm_vec3_normalize(cam->front);
+    camera.front[0] = cos(glm_rad(camera.yaw)) * cos(glm_rad(camera.pitch));
+    camera.front[1] = sin(glm_rad(camera.pitch));
+    camera.front[2] = sin(glm_rad(camera.yaw)) * cos(glm_rad(camera.pitch));
+    glm_vec3_normalize(camera.front);
 
     // Use new front vector to calculate right and up vectors
-    glm_vec3_cross(cam->front, cam->world_up, cam->right);
-    glm_vec3_normalize(cam->right);
+    glm_vec3_cross(camera.front, camera.world_up, camera.right);
+    glm_vec3_normalize(camera.right);
 
-    glm_vec3_cross(cam->right, cam->front, cam->up);
-    glm_vec3_normalize(cam->up);
+    glm_vec3_cross(camera.right, camera.front, camera.up);
+    glm_vec3_normalize(camera.up);
 }
 
-void camera_create(float pos_x, float pos_y, float pos_z, float up_x, float up_y, float up_z, float yaw, float pitch, float aspect_ratio, Camera* result){
-    result->position[0] = pos_x;
-    result->position[1] = pos_y;
-    result->position[2] = pos_z;
+void Camera_placeCamera(vec3 pos, vec3 up, vec3 front, float yaw, float pitch, float aspect_ratio)
+{
+    // Set vectors
+    glm_vec3_copy(pos, camera.position);    
+    glm_vec3_copy(up, camera.world_up);
+    glm_vec3_copy(front, camera.front);
+    
+    // Setting fields
+    camera.movement_speed = SPEED;
+    camera.mouse_sensitivity = SENSITIVITY;
+    camera.zoom = ZOOM;
+    camera.yaw = yaw;
+    camera.pitch = pitch;
+    camera.aspect_ratio = aspect_ratio;
+    camera.near_plane_dist = NEAR_PLANE_DIST;
+    camera.far_plane_dist = FAR_PLANE_DIST;
 
-    result->world_up[0] = up_x;
-    result->world_up[1] = up_y;
-    result->world_up[2] = up_z;
-
-    result->front[0] = 0.0f;
-    result->front[1] = 0.0f;
-    result->front[2] = -1.0f;
-
-    result->movement_speed = SPEED;
-    result->mouse_sensitivity = SENSITIVITY;
-    result->zoom = ZOOM;
-    result->yaw = yaw;
-    result->pitch = pitch;
-    result->aspect_ratio = aspect_ratio;
-    result->near_plane_dist = NEAR_PLANE_DIST;
-    result->far_plane_dist = FAR_PLANE_DIST;
-
-    update_camera_vectors(result); // Make sure vectors are up to date
+    _updateCameraVectors(); // Make sure vectors are up to date
 }
 
-void camera_get_view_matrix(Camera* cam, mat4 result){
-    vec3 sum; // Temp vector for storing sum of position and up vector
+void Camera_getProjectionMatrix(mat4 result)
+{
+    glm_perspective(glm_rad(camera.zoom), camera.aspect_ratio, camera.near_plane_dist, camera.far_plane_dist, result);
+}
+
+void Camera_getViewMatrix(mat4 result)
+{
+    vec3 sum; // Temp vector
     
     // Add position and up vector
-    glm_vec3_add(cam->position, cam->front, sum);
+    glm_vec3_add(camera.position, camera.front, sum);
 
     // Calculate view matrix and store in result
-    glm_lookat(cam->position, sum, cam->up, result);
+    glm_lookat(camera.position, sum, camera.up, result);
 }
 
-void camera_get_perspective(Camera* cam, mat4 result){
-    glm_perspective(glm_rad(cam->zoom), cam->aspect_ratio, cam->near_plane_dist, cam->far_plane_dist, result);
-}
-
-void camera_process_keyboard_movement(Camera* cam, CameraMovement movement, float delta_time){
+void Camera_processKeyboardMovement(Camera_MovementEnum movement, float delta_time)
+{
     // Calculate velocity
-    float velocity = cam->movement_speed * delta_time;
+    float velocity = camera.movement_speed * delta_time;
 
     // Update position based on direction and speed
     if(movement == FORWARD)
-        glm_vec3_muladds(cam->front,  velocity, cam->position);
+        glm_vec3_muladds(camera.front,  velocity, camera.position);
     if(movement == BACKWARD)
-        glm_vec3_muladds(cam->front, -velocity, cam->position);
+        glm_vec3_muladds(camera.front, -velocity, camera.position);
     if(movement == LEFT)
-        glm_vec3_muladds(cam->right, -velocity, cam->position);
+        glm_vec3_muladds(camera.right, -velocity, camera.position);
     if(movement == RIGHT)
-        glm_vec3_muladds(cam->right,  velocity, cam->position);
+        glm_vec3_muladds(camera.right,  velocity, camera.position);
 }
 
-void camera_process_mouse_movement(Camera* cam, float x_offset, float y_offset, unsigned char constrain_pitch){
+void Camera_processMouseMovement(float x_offset, float y_offset, unsigned char constrain_pitch)
+{
     // Scale offsets by sensitivity
-    x_offset *= cam->mouse_sensitivity;
-    y_offset *= cam->mouse_sensitivity;
+    x_offset *= camera.mouse_sensitivity;
+    y_offset *= camera.mouse_sensitivity;
 
     // Add offsets to pitch and yaw
-    cam->yaw += x_offset;
-    cam->pitch += y_offset;
+    camera.yaw += x_offset;
+    camera.pitch += y_offset;
 
     // Check pitch constraint
     if (constrain_pitch) {
-        if(cam->pitch > 89.0f)
-            cam->pitch = 89.0f;
-        if(cam->pitch < -89.0f)
-            cam->pitch = -89.0f;
+        if(camera.pitch > 89.0f)
+            camera.pitch = 89.0f;
+        if(camera.pitch < -89.0f)
+            camera.pitch = -89.0f;
     }
 
     // Update camera vectors
-    update_camera_vectors(cam);
+    _updateCameraVectors();
 }
 
-void camera_process_mouse_scroll(Camera* cam, float y_offset){
+void Camera_processMouseScroll(float y_offset)
+{
     // Add offset to zoom
-    cam->zoom -= y_offset;
+    camera.zoom -= y_offset;
 
     // Check zoom constraint
-    if(cam->zoom < 1.0f)
-        cam->zoom = 1.0f;
-    if(cam->zoom > 45.0f)
-        cam->zoom = 45.0f;
+    if(camera.zoom < 1.0f)
+        camera.zoom = 1.0f;
+    if(camera.zoom > 45.0f)
+        camera.zoom = 45.0f;
 }
