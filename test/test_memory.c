6b747a2b5671aa63c8e5f3a8a6a9c0aef179ff86
Thijs van Dijk <thijs3214@gmail.com>
2023-09-11 20:43:22 +0200

    Added memory test (just alligned for now)

---
+++ test/test_memory.c
@@ -0,0 +1,35 @@
+#include <tengine.h>
+#include <assert.h>
+
+#define NELEMENTS 128
+#define ALIGN 16
+
+int main()
+{
+    //----------------------------------------------------
+    // Testing Alligned
+    //----------------------------------------------------
+    vec4 *vectors = mem_aligned_create(NELEMENTS * sizeof(vec4), ALIGN);
+
+    // Test if actually aligned
+    assert((uintptr_t)vectors % ALIGN == 0);
+
+    // Test if shift value is in accepted range
+    assert(((unsigned char *)vectors)[-1] <= ALIGN);
+
+    float value = 0.0f;
+    for(size_t i = 0; i < NELEMENTS; i++)
+    {
+        vectors[i][0] = value;
+        vectors[i][1] = value;
+        vectors[i][2] = value;
+        vectors[i][3] = value;
+
+        value += 1.0f;
+    }
+
+    // Check if values are stored properly
+    assert(vectors[NELEMENTS - 1][3] == 127.0f);
+
+    mem_aligned_free(vectors);
+}
